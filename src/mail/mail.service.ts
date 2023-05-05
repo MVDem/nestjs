6fc3d4440f36c8985433535b36dfc43c91a812d0
Maxim Dementev <devmvdem@gmail.com>
2023-05-05 18:38:06 +0300

    HW4

---
+++ src/mail/mail.service.ts
@@ -0,0 +1,45 @@
+import { MailerService } from '@nestjs-modules/mailer';
+import { Injectable } from '@nestjs/common';
+import { CreateNewsDto } from 'src/news/dtos/create-news-dto';
+
+@Injectable()
+export class MailService {
+  constructor(private readonly mailerService: MailerService) {}
+  async sendTest() {
+    console.log('Отправляется письмо установки');
+    return this.mailerService
+      .sendMail({
+        to: 'news-blog-maxim@mail.ru',
+        subject: 'Первое тестовое письмо',
+        template: './test',
+      })
+      .then((res) => {
+        console.log('res', res);
+      })
+      .catch((err) => {
+        console.log('err', err);
+      });
+  }
+
+  async sendNewNewsForAdmins(
+    emails: string[],
+    news: CreateNewsDto,
+  ): Promise<void> {
+    console.log('Отправляются письма о новой новости администрации ресурса');
+    for (const email of emails) {
+      await this.mailerService
+        .sendMail({
+          to: email,
+          subject: `Создана новая новость: ${news.title}`,
+          template: './new-news',
+          context: news,
+        })
+        .then((res) => {
+          console.log('res', res);
+        })
+        .catch((err) => {
+          console.log('err', err);
+        });
+    }
+  }
+}
